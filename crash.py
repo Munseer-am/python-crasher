69f9b7bdb277b6f6d5f5ef8328b6799e829f0167
Munseer-am <82161602+Munseer-am@users.noreply.github.com>
2021-10-13 15:25:28 +0530

    Update crash.py

--- crash.py
+++ crash.py
@@ -6,6 +6,7 @@ import webbrowser
 # for loop
 for i in range(10000000000000000000000001):
     # opening websites
+    # opening google
     webbrowser.open_new_tab("https://www.google.com/")
   
     webbrowser.open_new_tab("https://www.facebook.com/")
