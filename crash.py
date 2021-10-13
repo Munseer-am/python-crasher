0c4625b556542857bc0ded3cc670a0991dbc6aea
Munseer-am <82161602+Munseer-am@users.noreply.github.com>
2021-10-13 15:25:56 +0530

    Update crash.py

--- crash.py
+++ crash.py
@@ -7,8 +7,10 @@ import webbrowser
 for i in range(10000000000000000000000001):
     # opening websites
     # opening google
+    
     webbrowser.open_new_tab("https://www.google.com/")
-  
+    
+    # opening facebook
     webbrowser.open_new_tab("https://www.facebook.com/")
    
     webbrowser.open_new_tab("https://www.instagram.com/")
